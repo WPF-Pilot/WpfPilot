cdff2a8d523eba3ba4a0ebd1b222cb616a284e53
Erik Lanning <cs@eriklanning.com>
2024-02-01 17:10:50 -0800

    Begin work on .NET 8.0 support

--- WpfPilot.Tests/TestUtility/CurrentFramework.cs
+++ WpfPilot.Tests/TestUtility/CurrentFramework.cs
@@ -1,9 +1,7 @@
 ﻿namespace WpfPilot.Tests.TestUtility;
 
 using System;
-using System.Diagnostics;
 using NUnit.Framework;
-using WpfPilot.Utility.WindowsAPI;
 
 public static class CurrentFramework
 {
@@ -12,6 +10,7 @@ public static class CurrentFramework
 		var cwd = TestContext.CurrentContext.WorkDirectory;
 		return cwd switch
 		{
+			_ when cwd.Contains("net8.0") => "net8.0-windows",
 			_ when cwd.Contains("net7.0") => "net7.0-windows",
 			_ when cwd.Contains("net6.0") => "net6.0-windows",
 			_ when cwd.Contains("net5.0") => "net5.0-windows",
