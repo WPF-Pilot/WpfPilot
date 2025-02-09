92b39656d005f1be6edf340413a8bdda18349ed9
ELanning <38930062+ELanning@users.noreply.github.com>
2025-02-08 18:22:56 -0800

    Support net 9.0
    * Support net 9.0
    
    * Bump Lib.Harmony

--- WpfPilot.Tests/TestUtility/CurrentFramework.cs
+++ WpfPilot.Tests/TestUtility/CurrentFramework.cs
@@ -10,13 +10,14 @@ public static class CurrentFramework
 		var cwd = TestContext.CurrentContext.WorkDirectory;
 		return cwd switch
 		{
+			_ when cwd.Contains("net9.0") => "net9.0-windows",
 			_ when cwd.Contains("net8.0") => "net8.0-windows",
 			_ when cwd.Contains("net7.0") => "net7.0-windows",
 			_ when cwd.Contains("net6.0") => "net6.0-windows",
 			_ when cwd.Contains("net5.0") => "net5.0-windows",
 			_ when cwd.Contains("net452") => "net452",
 			_ when cwd.Contains("netcoreapp") => "netcoreapp3.1",
-			_ => throw new NotSupportedException(),
+			_ => throw new NotSupportedException($"WPF Pilot {nameof(CurrentFramework)}.{nameof(GetVersion)} does not support this version."),
 		};
 	}
 }
